9fada1f765467392eb52d379a7b2810e9c037ca3
jyh928578@gmail.com <jyh928578@gmail.com>
2026-07-16 14:31:04 +0900

    패키지명

--- src/main/java/kr/co/ictedu/projectBack/controller/login/aop/LoginLoginAdvice.java
+++ src/main/java/kr/co/ictedu/projectBack/controller/login/aop/LoginLoginAdvice.java
@@ -1,4 +1,4 @@
-package kr.co.ictedu.projectBack.contoller.login.aop;
+package kr.co.ictedu.projectBack.controller.login.aop;
 
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
