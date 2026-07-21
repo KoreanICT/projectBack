7fb512d6fd4662b8e5f48ffe7f713c01fc8632cc
sol <test@test.com>
2026-07-21 17:21:55 +0900

    로그인

--- src/main/java/kr/co/ictedu/projectBack/dao/member/LoginDao.java
+++ src/main/java/kr/co/ictedu/projectBack/dao/member/LoginDao.java
@@ -2,6 +2,7 @@ package kr.co.ictedu.projectBack.dao.member;
 
 import java.util.Map;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -10,10 +11,10 @@ import kr.co.ictedu.projectBack.vo.MemberVO;
 
 @Mapper
 public interface LoginDao {
-	@Select("SELECT NAME, COUNT(*) cnt FROM MEMBER WHERE \r\n "
-			+ "email=#{email} and pwd=#{pwd} group by name")
+	@Select("SELECT MNUM, NAME, COUNT(*) cnt FROM MEMBER WHERE \r\n "
+			+ "email=#{email} and pwd=#{pwd} group by MNUM, name")
 	Map<String, Object> loginCheck(MemberVO vo);
-
+	
 }
 
 
