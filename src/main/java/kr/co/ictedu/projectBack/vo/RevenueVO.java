078aa47875246f0da986b5967d9fbc1bc5c412b7
seungpyo-heo <heoseungpyo416@gmail.com>
2026-07-23 18:23:37 +0900

    product backend

--- src/main/java/kr/co/ictedu/projectBack/vo/RevenueVO.java
+++ src/main/java/kr/co/ictedu/projectBack/vo/RevenueVO.java
@@ -10,10 +10,10 @@ import lombok.Setter;
 @Getter
 public class RevenueVO {
 
-	private String rmonth;
-	private int rtotalqty;
-	private int rtotalsales;
-	private int rtotalcost;
-	private int rtotalmargin;
+	private String rmonth;		// 매출 기준 월
+	private int rtotalqty;		// 총 판매 수량
+	private int rtotalsales;	// 총 매출액
+	private int rtotalcost;		// 총 원가
+	private int rtotalmargin;	// 총 마진
 	private int pnum;
 }
